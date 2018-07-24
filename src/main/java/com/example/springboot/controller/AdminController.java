b1e3fb1b9066d862f638654a48ab26c57e566d21
zyf <zyf@nbugs.com>
2018-07-24 17:34:17 +0800

    Merge remote-tracking branch 'remotes/origin/need1' into releases/20180724164749381_r_release_82897_springboot-code
    # Conflicts:
    #	src/main/java/com/example/springboot/controller/HelloController.java

---
+++ src/main/java/com/example/springboot/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zyf@nbugs.com
+ * @date 2018/7/24 下午5:33
+ * @description springboot
+ */
+@RestController
+public class AdminController {
+
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("test");
+        return "test ";
+    }
+}
